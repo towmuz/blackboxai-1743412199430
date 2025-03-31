99d396e038da76e746761293090a83c08ae75511
towmuz <towmuz@gmail.com>
2025-03-31 14:40:27 +0530

    Add logout.php 1743412226142

---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'includes/db.php';
+
+// Start session if not already started
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to login page
+header('Location: index.php');
+exit;
+?>
