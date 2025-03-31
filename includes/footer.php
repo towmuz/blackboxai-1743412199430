4ccdb6432f152513ea40db84dcc6af2957048ccf
towmuz <towmuz@gmail.com>
2025-03-31 14:40:31 +0530

    Add includes/footer.php 1743412230890

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+</div> <!-- Close container div from header -->
+    <footer class="bg-gray-800 text-white p-4 mt-8">
+        <div class="container mx-auto text-center">
+            <p>&copy; <?= date('Y') ?> Timetable Generator. All rights reserved.</p>
+        </div>
+    </footer>
+    <!-- Custom JavaScript -->
+    <script src="../assets/js/script.js"></script>
+</body>
+</html>
